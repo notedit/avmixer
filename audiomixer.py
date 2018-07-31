f880772197eb64f343f6b1b48e65b12a70a6690c
notedit <notedit@gmail.com>
2018-07-31 13:15:36 +0800

    add source remove

--- audiomixer.py
+++ audiomixer.py
@@ -9,18 +9,116 @@ gi.require_version('GLib', '2.0')
 gi.require_version('GObject', '2.0')
 from gi.repository import GLib, GObject, Gst
 
+GObject.threads_init()
+Gst.init(None)
 
+
+class AudioSource:
+
+    def __init__(self, freq, name):
+
+        self.src = Gst.ElementFactory.make('audiotestsrc', name)
+        self.src.set_property('freq',freq)
+        self.name = name
+        self.linked_pad = None
+
+
+class AudioMixer:
+
+    def __init__(self, loop):
+
+        pipe = Gst.Pipeline.new('mixer')
+        mixer = Gst.ElementFactory.make('audiomixer')
+        pipe.add(mixer)
+
+        audioconvert  = Gst.ElementFactory.make('audioconvert','audioconvert')
+        pipe.add(audioconvert)
+        mixer.link(audioconvert)
+
+        output = Gst.ElementFactory.make('autoaudiosink', 'audio_out')
+        pipe.add(output)
+        audioconvert.link(output)
+
+        self.loop = loop
+        self.pipe = pipe
+        self.mixer = mixer
+
+        self.sources = []
+
+        bus = pipe.get_bus()
+        bus.add_signal_watch()
+
+        bus.connect ('message', self._bus_call, loop)
+
+
+    def add_source(self, source):
+
+        sink_pad = self.mixer.get_request_pad('sink_%u')
+
+        src_pad  = source.src.get_static_pad('src')
+
+        self.pipe.add(source.src)
+        
+        src_pad.link(sink_pad)
+        
+        source.sink_pad = sink_pad
+        
+        source.src.sync_state_with_parent()
+
+        self.sources.append(source)
+
+
+
+    def remove_source(self, source):
+
+        if not source in self.sources:
+            return 
+
+        src_pad = source.src.get_static_pad('src')
+
+        source.src.set_state(Gst.State.NULL)
+
+        src_pad.unlink(source.sink_pad)
+
+        self.mixer.release_request_pad(source.sink_pad)
+
+        self.sources.remove(source)
+
+
+    def start(self):
+        self.pipe.set_state(Gst.State.PLAYING)
+
+
+    def stop(self):
+        self.pipe.set_state(Gst.State.NULL)
+        self.loop.quit()
+
+
+    def _bus_call(self, bus, message, loop):
+        t = message.type
+        if t == Gst.MessageType.EOS:
+            print('End-of-stream')
+            loop.quit()
+        elif t == Gst.MessageType.ERROR:
+            err, debug = message.parse_error()
+            print('Error: %s: %s\n' % (err, debug))
+            loop.quit()
+        return True
+
+        
 
 def add_source(data):
     pipe,mixer = data
 
-    sinkpad2 = mixer.get_request_pad('sink_%u')
-    buzzer2 = Gst.ElementFactory.make('audiotestsrc', 'buzzer2')
-    buzzer2.set_property('freq',1000)
-    pipe.add(buzzer2)
+    src2 = Gst.ElementFactory.make('audiotestsrc', 'src2')
+    src2.set_property('freq',1000)
 
-    buzzersrc2 = buzzer2.get_static_pad('src')
-    buzzersrc2.link(sinkpad2)
+    sink = mixer.get_request_pad('sink_%u')
+
+    pipe.add(src2)
+
+    src2pad = src2.get_static_pad('src')
+    src2pad.link(sink)
 
     buzzer2.sync_state_with_parent()
 
@@ -38,9 +136,9 @@ def bus_call(bus, message, loop):
         loop.quit()
     return True
 
+
+
 def main(args):
-    GObject.threads_init()
-    Gst.init(None)
 
     pipe = Gst.Pipeline.new('mixer')
     mixer = Gst.ElementFactory.make('audiomixer')
@@ -85,5 +183,40 @@ def main(args):
     # cleanup
     pipe.set_state(Gst.State.NULL)
 
+
+def remove_source(data):
+
+    print('remove_source')
+    
+    src2,mixer = data
+
+    mixer.remove_source(src2)
+
+    return True
+
+def test_mixer():
+
+    loop = GObject.MainLoop()
+
+    mixer = AudioMixer(loop)
+
+    src1 = AudioSource(500, 'src1')
+
+    mixer.add_source(src1)
+
+
+    src2 = AudioSource(1000, 'src2')
+
+    mixer.add_source(src2)
+
+    mixer.start()
+
+    GLib.timeout_add_seconds(5, remove_source, (src2, mixer))
+
+    loop.run()
+
+    mixer.stop()
+
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    #sys.exit(main(sys.argv))
+    test_mixer()
